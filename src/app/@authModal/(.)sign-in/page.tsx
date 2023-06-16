452caf678c5a2768b7c16eb9550c7d78cee587f5
Jian yong Lei <jianyonglei@gmail.com>
2023-06-16 16:27:30 -0400

    Database modeling

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -1,7 +1,7 @@
 import CloseModal from '@/components/CloseModal'
 import SignIn from '@/components/SignIn'
 
-const page = ({}) => {
+const page = ({ }) => {
   return <div className='fixed inset-0 bg-zinc-900/20 z-10'>
     <div className='container flex items-center h-full max-w-lg mx-auto'>
       <div className='relative bg-white w-full h-fit py-20 px-2 rounded-lg'>
--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { FC } from 'react'
 import { Button } from './ui/Button'
 import { X } from 'lucide-react'
 import { useRouter } from 'next/navigation'
