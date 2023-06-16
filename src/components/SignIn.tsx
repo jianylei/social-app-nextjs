05905c1b971fb57ba1497fb082caac54d5d6c7b1
Jian yong Lei <jianyonglei@gmail.com>
2023-06-16 03:27:07 -0400

    Added user avatar for nav

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,7 +3,6 @@ import { Icons } from './Icons'
 import Link from 'next/link'
 import UserAuthForm from './UserAuthForm'
 
-
 const SignIn: FC = () => {
   return <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
     <div className="flex flex-col space-y-2 text-center">
