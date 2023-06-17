bb40d877f6bb3cb89efe51f4c223350c77afb42e
Jian yong Lei <jianyonglei@gmail.com>
2023-06-17 18:56:52 -0400

    PATCH route for post vote

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -3,12 +3,13 @@ import { db } from "@/lib/db"
 import { redis } from "@/lib/redis"
 import { PostVoteValidator } from "@/lib/validators/vote"
 import { CachedPost } from "@/types/redis"
+import { z } from "zod"
 
 const CACHE_AFTER_UPVOTES = 1
 
 export async function PATCH(req: Request) {
     try {
-        const body = req.json()
+        const body = await req.json()
 
         const {postId, voteType} = PostVoteValidator.parse(body)
 
@@ -87,7 +88,44 @@ export async function PATCH(req: Request) {
 
             return new Response('OK')
         }
-    } catch (err) {
 
+        await db.vote.create({
+            data: {
+                type: voteType,
+                userId: session.user.id,
+                postId
+            }
+        })
+
+        // recount the votes
+        const votesAmt = post.votes.reduce((acc, vote) => {
+            if (vote.type === 'UP') return acc + 1
+            if (vote.type === 'DOWN') return acc - 1
+            return acc
+        }, 0)
+
+        if (votesAmt >= CACHE_AFTER_UPVOTES) {
+            const cachePayload: CachedPost = {
+                authorUsername: post.author.username ?? '',
+                content: JSON.stringify(post.content),
+                id: post.id,
+                title: post.title,
+                currentVote: voteType,
+                createdAt: post.createdAt
+            }
+
+            await redis.hset(`post:${postId}`, cachePayload)
+        }
+
+        return new Response('OK')
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            return new Response('Invalid POST request data passed', { status: 422 })
+        }
+
+        return new Response(
+            'Could not register your vote, please try again.',
+            { status: 500 }
+        )
     }
 }
--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -9,6 +9,7 @@ import { ArrowBigDown, ArrowBigUp } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { useMutation } from '@tanstack/react-query'
 import { PostVoteRequest } from '@/lib/validators/vote'
+import axios from 'axios'
 
 interface PostVoteClientProps {
   postId: string
@@ -30,18 +31,24 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
     setCurrentVote(initialVote)
   }, [initialVote])
 
-  const {} = useMutation({
+  const { mutate: vote } = useMutation({
     mutationFn: async (voteType: VoteType) => {
       const payload: PostVoteRequest = {
         postId,
         voteType
       }
+
+      await axios.patch('/api/subreddit/post/vote', payload)
     }
   })
 
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
-      <Button size="sm" variant="ghost" aria-label="upvote">
+      <Button
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+        onClick={() => vote('UP')}>
         <ArrowBigUp
           className={cn('h-5 w-5 text-zinc-700', {
             'text-emerald-500 fill-emerald-500': currentVote === 'UP'
@@ -53,7 +60,11 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
         {votesAmt}
       </p>
 
-      <Button size="sm" variant="ghost" aria-label="downvote">
+      <Button
+        size="sm"
+        variant="ghost"
+        aria-label="downvote"
+        onClick={() => vote('DOWN')}>
         <ArrowBigDown
           className={cn('h-5 w-5 text-zinc-700', {
             'text-red-500 fill-red-500': currentVote === 'DOWN'
