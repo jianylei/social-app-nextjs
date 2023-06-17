61e0a78d6bae394cf2f9c4349c4ec5c70142dbd4
Jian yong Lei <jianyonglei@gmail.com>
2023-06-17 18:41:49 -0400

    Confirgured redis caching

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,6 +1,8 @@
 import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
+import { redis } from "@/lib/redis"
 import { PostVoteValidator } from "@/lib/validators/vote"
+import { CachedPost } from "@/types/redis"
 
 const CACHE_AFTER_UPVOTES = 1
 
@@ -69,15 +71,22 @@ export async function PATCH(req: Request) {
                 if (vote.type === 'DOWN') return acc - 1
                 return acc
             }, 0)
-            /*
+
             if (votesAmt >= CACHE_AFTER_UPVOTES) {
-                const cachePayload = {
-
+                const cachePayload: CachedPost = {
+                    authorUsername: post.author.username ?? '',
+                    content: JSON.stringify(post.content),
+                    id: post.id,
+                    title: post.title,
+                    currentVote: voteType,
+                    createdAt: post.createdAt
                 }
-            }*/
+
+                await redis.hset(`post:${postId}`, cachePayload)
+            }
+
+            return new Response('OK')
         }
-
-
     } catch (err) {
 
     }
