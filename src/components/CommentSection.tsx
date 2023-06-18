653ae1c95164658e93d9d461f991222c1850d1a0
Jian yong Lei <jianyonglei@gmail.com>
2023-06-18 00:01:47 -0400

    Created reply comment components

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -54,8 +54,44 @@ const CommentSection = async ({ postId }: CommentSectionProps) => {
             return (
               <div key={topLevelComment.id} className="flex flex-col">
                 <div className="mb-2">
-                  <PostComment comment={topLevelComment} />
+                  <PostComment
+                    comment={topLevelComment}
+                    postId={postId}
+                    currentVote={topLevelCommentVote}
+                    votesAmt={topLevelCommentVotesAmt}
+                  />
                 </div>
+
+                {/* render replie */}
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length)
+                  .map((reply) => {
+                    const replyVotesAmt = topLevelComment.votes.reduce(
+                      (acc, vote) => {
+                        if (vote.type === 'UP') return acc + 1
+                        if (vote.type === 'DOWN') return acc - 1
+                        return acc
+                      },
+                      0
+                    )
+
+                    const replyVote = topLevelComment.votes.find(
+                      (vote) => vote.userId === session?.user.id
+                    )
+
+                    return (
+                      <div
+                        key={reply.id}
+                        className="ml-2 py-2 pl-4 border-l-2 border-zinc-200">
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    )
+                  })}
               </div>
             )
           })}
