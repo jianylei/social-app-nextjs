328b01ce214362a157b069ff01e825c59c8d8449
Jian yong Lei <jianyonglei@gmail.com>
2023-06-18 19:36:58 -0400

    fix: post card css

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -28,7 +28,7 @@ const Post: FC<PostProps> = ({
   const pRef = useRef<HTMLDivElement>(null)
 
   return (
-    <div className="rounded-md-bg-white shadow">
+    <div className="rounded-md bg-white shadow">
       <div className="px-6 py-4 flex justify-between">
         <PostVoteClient
           postId={post.id}
